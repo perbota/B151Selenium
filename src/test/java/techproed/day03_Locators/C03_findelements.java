e44a5637b572e8984fbb254a2317f2efeebaec99
Alper Küçük <perbotalp@gmail.com>
2023-06-08 20:10:19 +0300

    31/05/23

---
+++ src/test/java/techproed/day03_Locators/C03_findelements.java
@@ -0,0 +1,55 @@
+package techproed.day03_Locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+public class C03_findelements {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.Chrome.driver","src/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        //Amazon sayfasına gidelim
+        driver.get("https://amazon.com");
+
+        //sayfadaki linklerin sayısını ve linkleri yazdıralım
+        /*
+        findelement ile bir webelemente ulaşabilirken, birden fazla webelement için findelements() methodunu kullanırız.
+        Bu webelementlerin sayısına ulaşmak için ya da bu web elementlerin yazısını konsola yazdırabilmek için
+        List<Webelement> linklerLlistesi=driver.findelements(By.locator("locator değeri")) olarak kullanırız.
+        Oluşturmuş olduğumuz List'i loop ile konsola yazdırabiliriz
+        */
+        List<WebElement> linklerListesi = driver.findElements(By.tagName("a"));
+        System.out.println("Linklerin Sayısı = "+linklerListesi.size());
+/*
+        for (WebElement w:linklerListesi) {
+            if (!w.getText().isEmpty()) {
+                System.out.println(w.getText());
+            }
+        }
+
+ */
+        //Lambda ile yazdıralım
+        linklerListesi.forEach(link -> {if (!link.getText().isEmpty())
+        {System.out.println(link.getText());}});
+
+        //Shop deals in electronics webElementinin yazısını yazdıralım
+        System.out.println("*************************************************");
+        System.out.println(driver.findElement(By.id("nav-link-accountList-nav-line-1")).getText());
+        WebElement webElementYazisi = driver.findElement(By.id("nav-link-accountList-nav-line-1"));
+        System.out.println(webElementYazisi.getText());
+        //webelementin üzerindeki yazıyı almak istiyorsak getText() methodu kullanırız
+
+        driver.close();
+
+    }
+    //Amazon sayfasına gidiniz
+    //iphone aratınız
+    //çıkan sonuç yazısını konsola yazdırınız
+    //çıkan ürünlerden ilk 5 tanesine tıklayıp sayfa başlıklarını yazdırınız
+}
